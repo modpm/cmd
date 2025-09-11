689ca7b59c18951309764beecf8ee789d2ae3e6d
Zefir Kirilov <contact@zefir.pro>
2025-09-11 12:29:03 +0300

    document ANSI and document modules

--- src/cmd/ansi.d
+++ src/cmd/ansi.d
@@ -2,6 +2,7 @@ module cmd.ansi;
 
 import std.regex;
 
+/** Removes ANSI escape sequences from a string, leaving only the text that would be displayed. */
 public string stripAnsi(string input) @safe {
     return input.replaceAll(
         regex("\\x1B\\[[0-9;:?]*[A-Za-z]|\\x1B\\]8;;.*?\\x07(.*?)\\x1B\\]8;;\\x07|\\x1BO.|\\x1B.", "gs"),
@@ -9,18 +10,22 @@ public string stripAnsi(string input) @safe {
     );
 }
 
+/** Applies foreground (text) SGR bold (increased intensity). */
 public string bold(string text) nothrow @safe {
     return "\x1B[1m" ~ text ~ "\x1B[0m";
 }
 
+/** Applies foreground (text) SGR dim (faint, decreased intensity). */
 public string dim(string text) nothrow @safe {
     return "\x1B[2m" ~ text ~ "\x1B[0m";
 }
 
+/** Applies foreground (text) SGR bright black. */
 public string brightBlack(string text) nothrow @safe {
     return "\x1B[90m" ~ text ~ "\x1B[0m";
 }
 
+/** Formats text as OSC 8 hyperlink. */
 public string link(string text, string url) nothrow @safe {
     return "\x1B]8;;" ~ url ~ "\x07" ~ text ~ "\x1B]8;;\x07";
 }
--- src/cmd/document.d
+++ src/cmd/document.d
@@ -8,14 +8,33 @@ import std.typecons;
 
 import cmd.ansi;
 
+/** Represents a term with a definition. */
 public alias Term = Tuple!(string, string);
 
+/** Represents a section of a document. */
 public class Section {
+    /** Title of the section. */
     public const string title;
+
+    /** Main body text of the section, or `null`. */
     public const string body;
+
+    /** Terms and definitions associated with the section. */
     protected Term[] _terms;
+
+    /** The length of the longest term. */
     protected size_t longest = 0;
 
+    /**
+     * Constructs a new section.
+     *
+     * Params:
+     *   title = Title of the section.
+     *   body = Main body text of the section, or `null`.
+     *   terms = Terms and definitions associated with the section.
+     * Throws:
+     *   AssertionError if the title is null.
+     */
     public this(string title, string body = null, Term[] terms = []) @safe {
         assert(title !is null, "Section title cannot be null");
         this.title = title;
@@ -24,61 +43,98 @@ public class Section {
         longest = !terms.empty() ?terms.map!(t => t[0].stripAnsi().length).maxElement() : 0;
     }
 
+    /** Terms and definitions associated with the section. */
     public const(Term[]) terms() const nothrow @safe {
         return _terms;
     }
 
+    /** Adds a term to the section. */
     public Section add(Term term) @safe {
         _terms ~= term;
         longest = max(longest, term[0].stripAnsi().length);
         return this;
     }
 
+    /**
+     * Adds a term to the section.
+     *
+     * Params:
+     *   term = Term text.
+     *   definition = Definition text.
+     */
     public Section add(string term, string definition) @safe {
         return this.add(Term(term, definition));
     }
 }
 
+/**
+ * Represents a document comprised of sections.
+ */
 public class Document {
+    /** Sections associated with this document. */
     protected Section[] _sections;
 
+    /** Constructs a new document. */
     public this() nothrow @safe {
         _sections = [];
     }
 
+    /**
+     * Gets a section associated with this document by its title (ANSI is stripped).
+     *
+     * Params:
+     *   title = Title of the section.
+     * Returns:
+     *   Null if no matching section is found, otherwise the matching section.
+     */
     public const(Section) section(string title) const @safe {
         auto sections = this.sections().find!(s => s.title.stripAnsi() == title.stripAnsi());
         if (sections.empty()) return null;
         return sections.front();
     }
 
+    /** Gets the sections associated with this document. */
     public const(Section[]) sections() const nothrow @safe {
         return _sections;
     }
 
+    /** Adds a section to this document. */
     public Document add(Section section) nothrow @safe {
         _sections ~= section;
         return this;
     }
 
+    /**
+     * Adds a section to this document.
+     *
+     * Params:
+     *   title = Title of the section.
+     *   body = Body of the section.
+     *   terms = Terms associated with the section.
+     */
     public Document add(string title, string body = null, Term[] terms = []) @safe {
         return this.add(new Section(title, body, terms));
     }
 
+    /** Adds a term to a section matching the specified title (ANSI is stripped for matching). */
     public Document add(string section, Term term) @safe {
         auto sections = _sections.find!(s => s.title.stripAnsi() == section.stripAnsi());
         assert(!sections.empty(), "Could not find section " ~ section);
         sections.front().add(term);
         return this;
     }
-    
+
+    /** Adds a term to a section matching the specified title (ANSI is stripped for matching). */
     public Document add(string section, string term, string definition) @safe {
         return this.add(section, Term(term, definition));
     }
 
+    /**
+     * Prints this document to standard output.
+     */
     public void print() const @safe {
         const longest = sections().empty() ? 0 : sections().map!(s => s.longest).maxElement();
-        
+
         for (size_t i = 0; i < _sections.length; ++i) {
             const section = _sections[i];
             writeln(section.title);
