modpm/cmd

commit fb76e043892a110e624f1532e90b634761c5c716

added methods to get the parsed args without running the command

src/cmd/command.d
@@ -309,7 +309,7 @@ public class Command {
         exit(status);
     }
 
-    package int parse(string[] args, Program program) const {
+    package ParsedArgs parse(const(string[]) args, const(Program) program) const {
         if (!subcommands.empty()) {
             auto index = args.countUntil!(a => a.front() != '-');
             if (index >= 0) {
@@ -330,9 +330,9 @@ public class Command {
             string arg = args[i];
             if (arg.front() == '-') {
                 if (program.versionOption() !is null && program.versionOption().matches(arg))
-                    return program.printVersion();
+                    exit(program.printVersion());
                 if (program.helpOption() !is null && program.helpOption().matches(arg))
-                    return printHelp();
+                    exit(printHelp());
 
                 const(Flag) flag = findFlag(arg);
                 if (flag !is null) {
@@ -409,6 +409,10 @@ public class Command {
             }
         }
 
-        return _action(parsedArgs);
+        return parsedArgs;
+    }
+
+    package int run(const(string[]) args, const(Program) program) const {
+        return _action(parse(args, program));
     }
 }

src/cmd/program.d
@@ -188,12 +188,19 @@ public class Program : Command {
     }
 
     /**
+     * Gets the parsed arguments.
+     */
+    public ParsedArgs parse(string[] args) const {
+        return super.parse(args[1..$], this);
+    }
+
+    /**
      * Runs the program with the given arguments.
      *
      * Params:
      *   args = Command-line arguments array.
      */
-    public noreturn run(string[] args) {
-        exit(super.parse(args[1..$], this));
+    public noreturn run(string[] args) const {
+        exit(super.run(args[1..$], this));
     }
 }

commit 9fddc707553bab7f46e2eb69836bc01ebcb5545f

fix `Command.run()` not delegating to sub-commands Bug was introduced in fb76e043892a110e624f1532e90b634761c5c716.
The problem is that `parse()` delegates parsing to sub-commands, but
since it's now decoupled from `run()`, running was no longer delegated.

Accessing the delegated command during parsing via `ParsedArgs#command`.

src/cmd/command.d
@@ -405,7 +405,8 @@ public class Command {
         return parsedArgs;
     }
 
-    package int run(const(string[]) args, const(Program) program) const {
-        return _action(parse(args, program));
+    public int run(const(string[]) args, const(Program) program) const {
+        auto parsed = parse(args, program);
+        return parsed.command._action(parsed);
     }
 }